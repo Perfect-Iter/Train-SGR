8d2f89133fff4a2edccd98a3ced77124a031c8a2
ruben <barazaruben.br@gmail.com>
2021-03-18 15:50:17 +0300

    updated create to push to database

--- app/src/main/java/com/example/train_srgticket/LoginActivity.kt
+++ app/src/main/java/com/example/train_srgticket/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.train_srgticket
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.example.train_srgticket.databinding.ActivityLoginBinding
+import org.json.JSONObject
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 private lateinit var binding: ActivityLoginBinding
@@ -19,25 +29,82 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.buttonLogIn.setOnClickListener {
-            val toMain = Intent(applicationContext, MainScreenActivity::class.java)
+            //val toMain = Intent(applicationContext, MainScreenActivity::class.java)
+            //startActivity(toMain)
 
-            val user = binding.inputUser.text.toString()
-            val password = binding.inputPassword.text.toString()
-
-            if(user !="" && password!=""){
-                if(user == "12345" && password == "admin"){
-                    startActivity(toMain)
-                } else{
-                    Toast.makeText(applicationContext, "Invalid credentials", Toast.LENGTH_SHORT).show()
-                }
-            }else{
-                Toast.makeText(applicationContext, "Please Enter credentials", Toast.LENGTH_SHORT).show()
-            }
+            val login = MyAsyncTask(applicationContext)
+            login.execute()
 
         }
         binding.textSignin.setOnClickListener{
             val toSignIn = Intent(applicationContext, signUpActivity::class.java)
             startActivity(toSignIn)
         }
+    }
+
+    companion object {
+        class MyAsyncTask internal constructor(context: Context) : AsyncTask<String, String, String>() {
+            lateinit var con:HttpURLConnection
+            lateinit var resulta:String
+            val builder = Uri.Builder()
+            private val cont:Context=context
+            override fun onPreExecute() {
+                super.onPreExecute()
+
+                val progressBar= ProgressBar(cont)
+                progressBar.isIndeterminate=true
+                progressBar.visibility= View.VISIBLE
+
+                val phone: Int = binding.inputPhone.text.toString().toInt()
+                val pass: String = binding.inputPassword.text.toString()
+
+                builder .appendQueryParameter("phone", phone.toString())
+                builder .appendQueryParameter("password", pass)
+                builder .appendQueryParameter("key", "oooo")
+            }
+
+            override fun doInBackground(vararg params: String?):  String? {
+                try {
+
+                    var query = builder.build().encodedQuery
+                    val url: String = "http://sgrticket96.000webhostapp.com/sgr/read.php"
+                    val obj = URL(url)
+                    con = obj.openConnection() as HttpURLConnection
+                    con.setRequestMethod("GET")
+                    con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)")
+                    con.setRequestProperty("Accept-Language", "UTF-8")
+                    con.setDoOutput(true)
+                    val outputStreamWriter = OutputStreamWriter(con.getOutputStream())
+                    outputStreamWriter.write(query)
+                    outputStreamWriter.flush()
+                    Log.e("pass 1", "connection success ")
+                } catch (e: Exception) {
+                    Log.e("Fail 1", e.toString())
+                }
+                try {
+                    resulta = con.inputStream.bufferedReader().readText()
+                    Log.e("data", resulta)
+                } catch (e: java.lang.Exception) {
+                    Log.e("Fail 2", e.toString())
+                }
+                fun onPostExecute(result: String?) {
+                    super.onPostExecute(result)
+
+                    var json_data = JSONObject(resulta)
+                    val code: Int = json_data.getInt("code")
+                    Log.e("data",code.toString())
+                    if (code == 1) {
+//                        val com: JSONArray = json_data.getJSONArray("userdetails")
+//                        val comObject = com[0] as JSONObject
+//                        Log.e("data",""+comObject.optString("fname"))
+                        val toMain = Intent(cont, MainScreenActivity::class.java)
+                        cont.startActivity(toMain)
+                    }
+                }
+
+                return null;
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/train_srgticket/signUpActivity.kt
+++ app/src/main/java/com/example/train_srgticket/signUpActivity.kt
@@ -1,10 +1,20 @@
 package com.example.train_srgticket
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import com.example.train_srgticket.databinding.ActivitySignUpBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 private lateinit var binding: ActivitySignUpBinding
 
@@ -18,8 +28,11 @@ class signUpActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.buttonSignUp.setOnClickListener {
-            val toMainScreen = Intent(applicationContext, MainScreenActivity::class.java)
-            startActivity(toMainScreen)
+                val tosignup = SignUpAsyncTask(applicationContext)
+                tosignup.execute()
+                val toMain = Intent(applicationContext, MainScreenActivity::class.java)
+                startActivity(toMain)
+
         }
 
         binding.textLogin.setOnClickListener {
@@ -29,7 +42,68 @@ class signUpActivity : AppCompatActivity() {
 
     }
 
-    fun openLogin(){
+    companion object {
+        class SignUpAsyncTask internal constructor(context: Context) : AsyncTask<String, String, String>() {
+            lateinit var con: HttpURLConnection
+            lateinit var resulta:String
+            val builder = Uri.Builder()
+            private val cont: Context =context
+            override fun onPreExecute() {
+                super.onPreExecute()
+                val name: String = binding.addName.text.toString()
+                val phone: Int = binding.addPhone.text.toString().toInt()
+                val pass: String = binding.addPassword.text.toString()
+
+                val progressBar= ProgressBar(cont)
+                progressBar.isIndeterminate=true
+                progressBar.visibility= View.VISIBLE
+                builder .appendQueryParameter("name", name)
+                builder .appendQueryParameter("phone", phone.toString())
+                builder .appendQueryParameter("password", pass)
+                builder .appendQueryParameter("key", "oooo")
+            }
+
+            override fun doInBackground(vararg params: String?):  String? {
+                try {
+
+                    var query = builder.build().encodedQuery
+                    val url: String = "http://sgrticket96.000webhostapp.com/sgr/create.php"
+                    val obj = URL(url)
+                    con = obj.openConnection() as HttpURLConnection
+                    con.setRequestMethod("POST")
+                    con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)")
+                    con.setRequestProperty("Accept-Language", "UTF-8")
+                    con.setDoOutput(true)
+                    val outputStreamWriter = OutputStreamWriter(con.getOutputStream())
+                    outputStreamWriter.write(query)
+                    outputStreamWriter.flush()
+                    Log.e("pass 1", "connection success ")
+                } catch (e: Exception) {
+                    Log.e("Fail 1", e.toString())
+                }
+                try {
+                    resulta = con.inputStream.bufferedReader().readText()
+                    Log.e("data", resulta)
+                } catch (e: java.lang.Exception) {
+                    Log.e("Fail 2", e.toString())
+                }
+                fun onPostExecute(result: String?) {
+                    super.onPostExecute(result)
+
+                    var json_data = JSONObject(resulta)
+                    val code: Int = json_data.getInt("code")
+                    Log.e("data",code.toString())
+                    if (code == 1) {
+                        val com: JSONArray = json_data.getJSONArray("userdetails")
+                        val comObject = com[0] as JSONObject
+                        Log.e("data",""+comObject.optString("fname"))
+
+                    }
+                }
+
+                return null;
+            }
+        }
 
     }
 }
